2a9ae6403d45a7f5d99ea2a548c09b47282224e8
EkateriniTri <136068628+EkateriniTri@users.noreply.github.com>
2025-04-23 18:27:41 +0200

    Created the basic CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
+import "./Weather.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
+      <Weather />
+
+      <footer div className="Footer">
         This project was coded by{""} Katerina Tri {""}and is {""}{" "}
         <a
           href="https://github.com/EkateriniTri/react-weather-app.git"
