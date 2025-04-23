6ff2b69bd4bfa7b7d834fbbebffd829b3ab46622
EkateriniTri <136068628+EkateriniTri@users.noreply.github.com>
2025-04-23 16:16:03 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{""} Katerina Tri {""}and is {""}{" "}
+        <a
+          href="https://github.com/EkateriniTri/react-weather-app.git"
+          target="_blank"
+          rel="noopener noreferrer">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
