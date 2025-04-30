49ef6eaebc47ee3b8fdbb6e48517587b2e8c5db4
EkateriniTri <136068628+EkateriniTri@users.noreply.github.com>
2025-04-30 11:31:19 +0200

    Display the relevant temperature in different units

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -19,7 +19,7 @@ export default function WeatherInfo(props) {
             <div className="float-left">
               <WeatherIcon code={props.data.icon} size={90} />
             </div>
-            <div>
+            <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
