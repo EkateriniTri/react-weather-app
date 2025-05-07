f44bb0a0f5a788b2e788b3cbfb78f1ac4b9a466b
EkateriniTri <136068628+EkateriniTri@users.noreply.github.com>
2025-05-07 18:51:26 +0200

    building the Forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -7,32 +7,35 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
+
   if (loaded) {
+    console.log(props);
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.map(function (dailyForecast, index) {
+          {forecast.map(function (day, index) {
             if (index < 5) {
               return (
                 <div className="col" key={index}>
-                  <WeatherForecastDay data={dailyForecast} />
+                  <WeatherForecastDay data={day} />
                 </div>
               );
             }
           })}
-          <div className="col"></div>
         </div>
       </div>
     );
   } else {
     let apiKey = "6c4a1fa1oe455fb969c4b3876f90t341";
-    let longitude = props.coordinates.longitude;
-    let latitude = props.coordinates.latitude;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
 
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,18 +1,18 @@
 import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import WeatherForecastDayIcon from "./WeatherForecastDayIcon";
 
-export default function WeatherForecastDay(pros) {
+export default function WeatherForecastDay(props) {
   function maxTemperature() {
-    let temperature = Math.round(props.data.daily.temperature.maximum);
+    let temperature = Math.round(props.data.temperature.maximum);
     return `${temperature}°`;
   }
   function minTemperature() {
-    let temperature = Math.round(props.data.daily.temperature.minimum);
+    let temperature = Math.round(props.data.temperature.minimum);
     return `${temperature}°`;
   }
 
   function day() {
-    let date = new Date(props.data.dt * 1000);
+    let date = new Date(props.data.time * 1000);
     let day = date.getDay();
 
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -21,10 +21,10 @@ export default function WeatherForecastDay(pros) {
   }
 
   return (
-    <div>
-      <div className="WeatherForecast-day">{day()}</div>
-      <WeatherIcon code={props.data.daily.condition[0].icon} size={36} />
-      <div className="WeatherForecast-temperatures">
+    <div className="WeatherForecast">
+      <div className="WeatherForecast-day">{day()} </div>
+      <WeatherForecastDayIcon code={props.data.condition.icon} size={35} />
+      <div className="WeatherForecast-temperature">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}
         </span>
